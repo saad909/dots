c7f3597f255319d9d9ec117308de43bbcdb76326
saad909 <69252484+saad909@users.noreply.github.com>
2020-10-31 02:40:14 +0500

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,18 @@
+7z x config.zip
+#Creating directories
+mkdir -p {/home/$USER/.local/bin,/home/$USER/.config,/home/$USER/.local/share/fonts}
+#copy configs
+# .config
+cp  ~/installation/dots/config/* ~/.config --recursive
+sudo chmod +wrx ~/.config/polybar/launch.sh
+# home
+cp  ~/installation/dots/home/. ~/ --recursive
+# fonts
+cp ~/installation/dots/fonts/* ~/.local/share/fonts  --recursive
+#scripts
+cp ~/installation/dots/scripts/* ~/.local/bin  --recursive
+sudo chmod +wrx ~/.local/bin/* --recursive
+# Changing ownership
+sudo chown $USER /home/* --recursive
+
+
